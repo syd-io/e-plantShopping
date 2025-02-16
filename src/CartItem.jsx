0adfe5e228a04b3abba34e389e781d56bb4c47ed
Isa Alhadad <sy.isahd@gmail.com>
2025-02-17 00:37:38 +0800

    fixed navigation, added total plants and checkout alert

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -6,6 +6,7 @@ import './CartItem.css';
 const CartItem = ({ onContinueShopping }) => {
   const cart = useSelector(state => state.cart.items);
   const dispatch = useDispatch();
+  const totalPlants = cart.reduce((total, item) => total + item.quantity, 0);
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
@@ -50,6 +51,7 @@ const CartItem = ({ onContinueShopping }) => {
   return (
     <div className="cart-container">
       <h2 style={{ color: 'black' }}>Total Cart Amount: ${calculateTotalAmount()}</h2>
+      <h2 style={{ color: 'black' }}>Total Plants: {totalPlants}</h2>
       <div>
         {cart.map(item => (
           <div className="cart-item" key={item.name}>
@@ -72,7 +74,7 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={(e) => handleCheckoutShopping(e)}>Checkout</button>
       </div>
     </div>
   );
